49192e5c57b7e9016f5d2a83a6b21ea31bf7e46d
sweetjoohyun <joohyun@sqisoft.com>
2022-06-21 16:49:15 +0900

    PublicRoute / PrivateRoute 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 import Theme from "./component/theme/Theme";
 import Layout from "./component/layout/Layout";
+import Main from "./view/Main";
+import PrivateRoute from "./component/routes/PrivateRoute";
+import PublicRoute from "./component/routes/PublicRoute";
+import { getPath } from "./utils/user";
+
 
 function App() {
-    return (
-        <Theme>
-            <BrowserRouter>
-                <Layout/>
-            </BrowserRouter>
-        </Theme>
-    );
+  return (
+    <Theme>
+      <BrowserRouter>
+        { getPath() ? ( <PrivateRoute component={Layout} /> )
+          : ( <PublicRoute path="/" component={Main} /> )}
+      </BrowserRouter>
+    </Theme>
+  );
 }
 
 export default App;
