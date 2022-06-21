8fd94ee7ad996356518bbd16bf7edf942b663e20
sweetjoohyun <joohyun@sqisoft.com>
2022-06-21 16:48:47 +0900

    out 버튼 추가

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -1,47 +1,70 @@
 import React from "react";
-import {makeStyles} from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
 import PopperMenu from "../menu/PopperMenu";
+import { ExitToApp } from "@material-ui/icons";
+import { removePathSession } from "../../utils/user";
 
 function Header() {
-    const classes = useStyles()
+  const classes = useStyles()
 
-    return (
-        <div className={classes.root}>
-            <div className={classes.empty}></div>
-            <div className={classes.menu}>
-                <PopperMenu menu={'이미지'} menuItems={imageMenuItems}/>
-            </div>
-            <div className={classes.menu}>
-                <PopperMenu menu={'영상'} menuItems={videoMenuItems}/>
-            </div>
+  const handleOut = () => {
+    removePathSession()
+    window.location = '/';
+  }
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.menuArea}>
+        <div className={classes.empty}></div>
+        <div className={classes.menu}>
+          <PopperMenu menu={'이미지'} menuItems={imageMenuItems}/>
         </div>
-    )
+        <div className={classes.menu}>
+          <PopperMenu menu={'영상'} menuItems={videoMenuItems}/>
+        </div>
+      </div>
+      <div className={classes.out}>
+        <Button onClick={handleOut}>
+          <ExitToApp />
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 const imageMenuItems = [
-    {name: '객체검출'},
-    {name: '객체검출2'},
-    {name: '객체검출3'}
+  {name: '객체검출'},
+  {name: '객체검출2'},
+  {name: '객체검출3'}
 ]
 
 const videoMenuItems = [
-    {name: '객체검출'},
-    {name: '객체검출2'},
-    {name: '객체검출3'}
+  {name: '객체검출'},
+  {name: '객체검출2'},
+  {name: '객체검출3'}
 ]
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        backgroundColor: theme.base.headerColor,
-        minHeight: theme.base.headerHeight,
-        display: "flex",
-    },
-    empty: {
-        width: theme.spacing(10)
-    },
-    menu: {
-        marginRight: theme.spacing(1),
-    }
+  root: {
+    backgroundColor: theme.base.headerColor,
+    minHeight: theme.base.headerHeight,
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  menuArea:{
+    display: "flex",
+
+  },
+  empty: {
+    width: theme.spacing(10)
+  },
+  menu: {
+    marginRight: theme.spacing(1),
+  },
+  out:{
+    display: "flex",
+
+  }
 }))
 
 export default Header;
