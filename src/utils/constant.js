c8d37920cbba70f8bc1cc31d22ba85becd45cbd5
sweetjoohyun <joohyun@sqisoft.com>
2022-06-22 17:37:00 +0900

    constant.js 추가

---
+++ src/utils/constant.js
@@ -0,0 +1,11 @@
+
+export const imageTitle = '이미지';
+export const videoTitle = '영상';
+
+export const objectDetectionTitle = '객체검출';
+
+
+export const imageExtension = '.jpg, .jpeg, .png';
+export const videoExtension = '.mp4, .avi, .av1';
+
+export const messageNotImage = 'jpg, jpeg, png 파일이 아닙니다.';
