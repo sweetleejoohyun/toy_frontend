6e93f1bca88a6cab407837ea86154d54a00f0518
sweetjoohyun <joohyun@sqisoft.com>
2022-07-11 17:29:29 +0900

    OpenFileButton 으로 변경 동영상업로드 API 호출 추가 progressbar 추가 컴포넌트 추가로 스타일 수정

--- src/component/panel/VideoPanel.js
+++ src/component/panel/VideoPanel.js
@@ -1,39 +1,84 @@
 import React, { useState } from "react";
 import {
-  Button,
-  Collapse,
+  Box,
+  Collapse, LinearProgress,
   makeStyles,
   Typography
 } from "@material-ui/core";
-import {
-  FolderOpenOutlined,
-} from "@material-ui/icons";
 import Alert from "@material-ui/lab/Alert";
+import axios from "axios";
 
 import {
   videoExtension,
   videoTitle,
   messageNotVideo,
-  objectDetectionTitle, messageRestrictVideoSize,
+  objectDetectionTitle,
+  messageRestrictVideoSize,
 } from "../../common/Constant";
 import VideoPlayer from "../player/VideoPlayer";
+import OpenFileButton from "../button/OpenFileButton";
+import appConfig from "../../common/AppConfig";
+import {onAxiosError} from "../../common/Error";
 
 
 function VideoPanel(){
   const classes = useStyles();
   const [openAlert, setOpenAlert] = useState(false);
+  const [alertMsg, setAlertMsg] = useState('');
   const [selectedFile, setSelectedFile] = useState();
+  const [displayProgress, setDisplayProgress] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const onChangeFile = event => {
     const file = event.target.files[0]
     const extArr = videoExtension.split(',')
-    // check the extension
-    if (extArr.find(element => file.name.endsWith(element)) && file.size < 1600000) {
-      setSelectedFile(file);
-      setOpenAlert(false)
-    } else {
+    // check file size
+    if (file.size > 2097152) {
+      setAlertMsg(messageRestrictVideoSize)
+      setSelectedFile()
       setOpenAlert(true)
+    } else {
+      // check the extension
+      if (extArr.find(element => file.name.endsWith(element))) {
+        setOpenAlert(false)
+        uploadVideo(file)
+      } else {
+        setAlertMsg(messageNotVideo)
+        setSelectedFile()
+        setOpenAlert(true)
+      }
     }
+  };
+
+  // upload video
+  const uploadVideo = (file) => {
+    setDisplayProgress(true);
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('video', file);
+
+    axios
+      .post(appConfig.apiRoot + '/video/upload', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+        onUploadProgress: progressEvent => {
+          const { loaded, total } = progressEvent;
+          let percentage = Math.floor((loaded * 100) / total);
+          setProgress(percentage);
+        },
+      })
+      .then(response => {
+        if (response.status === 200) {
+          console.log(response.data.message)
+          setSelectedFile(file);
+        }
+      })
+      .catch(error => {
+        setSelectedFile()
+        onAxiosError(error);
+      })
+      .finally( () => {
+        // setDisplayProgress(false)
+      })
   };
 
   return(
@@ -43,24 +88,36 @@ function VideoPanel(){
       </div>
       <div className={classes.fileDiv}>
         <div className={classes.fileTitle}>
-          <Button className={classes.button} component="label">
-            <FolderOpenOutlined/>
-            <Typography className={classes.fileTypo}>{'영상 열기'}</Typography>
-            <input
-              type="file"
-              onChange={onChangeFile}
-              style={{ display: 'none' }}
-              accept={videoExtension}
-            />
-          </Button>
-          <Collapse in={openAlert} style={!openAlert && {display: 'none'}}>
-            <Alert severity="error">{messageNotVideo}</Alert>
-          </Collapse>
+          <OpenFileButton
+            name={'영상 열기'}
+            onChangeFile={onChangeFile}
+            accept={videoExtension}/>
+
           { selectedFile?
-            <Typography className={classes.filenameTypo}>{selectedFile.name}</Typography> : null }
+            <Typography className={classes.filenameTypo}>{selectedFile.name}</Typography> : null
+          }
+          { displayProgress?
+            <div className={classes.progressDiv}>
+              <Box sx={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+                <Box sx={{ width: '100%', mr: 1 }}>
+                  <LinearProgress variant="determinate" value={progress} />
+                </Box>
+                <Box sx={{ minWidth: 35 }}>
+                  <label className={classes.label}>
+                    {`${Math.round(progress)}%`}
+                  </label>
+                </Box>
+              </Box>
+            </div> : null
+          }
         </div>
         <div>
           <Typography  className={classes.fileDesc}> { messageRestrictVideoSize }</Typography>
+        </div>
+        <div>
+          <Collapse in={openAlert}>
+            <Alert severity="error">{alertMsg}</Alert>
+          </Collapse>
         </div>
       </div>
       <div className={classes.videoDiv}>
@@ -75,6 +132,7 @@ function VideoPanel(){
 
 const useStyles = makeStyles(theme => ({
   root:{
+    height: '100%',
   },
   topDiv:{
     display: "flex",
@@ -100,15 +158,6 @@ const useStyles = makeStyles(theme => ({
     color: "red",
     fontSize: theme.spacing(1.5),
   },
-  button:{
-    fontFamily: theme.base.fontFamily,
-    fontSize: theme.spacing(2),
-    marginRight: theme.spacing(1),
-    paddingLeft: theme.spacing(0),
-  },
-  fileTypo:{
-    marginLeft: theme.spacing(1)
-  },
   filenameTypo: {
     display: "flex",
     alignItems: "center",
@@ -117,7 +166,7 @@ const useStyles = makeStyles(theme => ({
   },
   videoDiv:{
     // width: '50vw',
-    height: '75vh',
+    height: '70vh',
     display: 'flex',
     justifyContent: "center",
     marginLeft: theme.spacing(1),
@@ -125,6 +174,13 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(1),
     marginBottom: theme.spacing(2),
   },
+  progressDiv:{
+    marginLeft: theme.spacing(3),
+    width: '60%',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  }
 }));
 
 export default VideoPanel;
