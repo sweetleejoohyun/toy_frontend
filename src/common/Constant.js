ac20592d4935dc9fb4ccb63a1a4866c9c9e9ea46
sweetjoohyun <joohyun@sqisoft.com>
2022-07-11 17:26:03 +0900

    업로드가능한 동영상형식 추가 동영상크기 1.5MB -> 2.0MB 변경

--- src/common/Constant.js
+++ src/common/Constant.js
@@ -5,10 +5,10 @@ export const videoTitle = '영상';
 export const objectDetectionTitle = '객체검출';
 
 
-export const imageExtension = '.jpg, .jpeg, .png';
-export const videoExtension = '.mp4, .avi, .av1';
+export const imageExtension = '.jpg,.jpeg,.png';
+export const videoExtension = '.mp4,.avi,.mkv,.webm';
 
 export const messageNotImage = 'jpg, jpeg, png 파일이 아닙니다.';
-export const messageNotVideo = 'mp4, avi, av1 파일이 아닙니다.';
+export const messageNotVideo = 'mp4, avi, mkv, webm 파일이 아닙니다.';
 
-export const messageRestrictVideoSize = '영상의 크기는 1.5MB를 넘을 수 없습니다.';
+export const messageRestrictVideoSize = '영상의 크기는 2.0MB를 넘을 수 없습니다.';
