a038ee014331585403c722c9fd4bbb9afe707616
sweetjoohyun <joohyun@sqisoft.com>
2022-07-13 19:32:26 +0900

    object Detection Models 추가

--- src/common/Constant.js
+++ src/common/Constant.js
@@ -11,3 +11,5 @@ export const messageNotVideo = 'mp4, avi, mkv, webm 파일이 아닙니다.';
 export const messageRestrictVideoSize = '영상의 크기는 2.0MB를 넘을 수 없습니다.';
 
 export const imageBaseSize = 700;
+
+export const objectDetectionModels = ['SsdMobilenetV2','FasterRcnnResnetV2'];
